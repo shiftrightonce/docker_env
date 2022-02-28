dd33f3911fa2db402372fab2ca8e55de2bc8a478
Jibao Mansaray <jibaomansaray@gmail.com>
2022-02-28 09:05:57 +0000

    Remove respositories that are private

--- clone_repos.sh
+++ clone_repos.sh
@@ -7,53 +7,6 @@ cd projects
 mkdir --verbose faith_packages
 mkdir --verbose databases
 
-# Clone the crm
-git clone ssh://git@git.faithcsde.com:2222/faith/faithcrm.git crm
-# Move into the crm folder
-cd crm
-# Check out dev channel
-git checkout dev
-# Move back to the projects folder
-cd ..
-
-# Clone the api
-git clone ssh://git@git.faithcsde.com:2222/faith/api.git
-# Move into the api folder
-cd api
-# Check out the dev channel
-git checkout dev
-# Move back to the projects folder
-cd ..
-
-# Clone the website
-git clone ssh://git@git.faithcsde.com:2222/faith/faithwebsite.git fweb
-# Move into the fweb folder
-cd fweb
-# Check out the dev channel
-git checkout dev
-# Move back to the projects folder
-cd ..
-
-# Clone Geoff's Website Fork
-git clone ssh://git@git.faithcsde.com:2222/GCaldL/faithwebsite.git gweb
-
-# Clone Caleb's Website Fork
-git clone ssh://git@git.faithcsde.com:2222/Caleb_Suthers/faithwebsite.git cweb
-
-# Move into the faith_packages folder
-cd faith_packages
-# Clone Caleb's uteachme package
-git clone ssh://git@git.faithcsde.com:2222/Caleb_Suthers/uteachme_package.git uteachme
-# Move back to the projects folder
-cd ..
-
-# Move into the databases folder
-cd databases
-# Download adminer
-wget https://github.com/vrana/adminer/releases/download/v4.7.7/adminer-4.7.7.php
-# Rename the downloaded file
-mv adminer*.php index.php
-
 # Get into phpfpm container
 docker-compose exec phpfpm bash
 
