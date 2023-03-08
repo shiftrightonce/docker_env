f7118ec4dc1ea8165a61434f75b8efe20d44a4b8
Jibao Mansaray <jibaomansaray@gmail.com>
2023-03-09 09:17:08 +1000

    Rename .env.example to env.example
    Makes the file visiable on linux systems by default.

--- dev.sh
+++ dev.sh
@@ -8,7 +8,7 @@ then
       mkdir -p .key
       mkdir -p projects
       cp /home/${USER}/.ssh/id_rsa ./.key/id_rsa
-      cp .env.example .env
+      cp env.example .env
       docker compose build
    fi
 else
