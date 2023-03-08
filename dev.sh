c4970ff8d3dfd31d774f59627ed27997b3b5268d
Jibao Mansaray <jibaomansaray@gmail.com>
2023-03-09 08:56:18 +1000

    Rename `docker-compose` to `docker compose`

--- dev.sh
+++ dev.sh
@@ -9,14 +9,14 @@ then
       mkdir -p projects
       cp /home/${USER}/.ssh/id_rsa ./.key/id_rsa
       cp .env.example .env
-      docker-compose build
+      docker compose build
    fi
 else
     echo "---> Quick help <----"
     echo "setup : Setup your environment for the first time".
-    echo "docker-compose up : Starts the containers"
-    echo "docker-compose exec db bash : ssh into the mysql container"
-    echo "docker-compose exec phpfpm bash : ssh into the php environment. This is where you should run composer"
-    echo "docker-compose exec nginx bash: ssh into the web server container"
-    echo "docker-compse exec workspace bash : ssh into the general container"
+    echo "docker compose up : Starts the containers"
+    echo "docker compose exec db bash : ssh into the mysql container"
+    echo "docker compose exec phpfpm bash : ssh into the php environment. This is where you should run composer"
+    echo "docker compose exec nginx bash: ssh into the web server container"
+    echo "docker compse exec workspace bash : ssh into the general container"
 fi
