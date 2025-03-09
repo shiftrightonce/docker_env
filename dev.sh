fb4f986e3055fe9f8174c97acbb2d5514d84d3f0
Jibao Mansaray <jibaomansaray@gmail.com>
2025-03-09 19:36:34 +1000

    fix surrealdb missing data folder

--- dev.sh
+++ dev.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]
-then
-   if [ $1 = 'setup' ]
-   then
-      echo '--- setting up your dev environment ---'
-      mkdir -p .key
-      mkdir -p projects
-      cp /home/${USER}/.ssh/id_rsa ./.key/id_rsa
-      cp env.example .env
-      docker compose build
-   fi
+if [ $# -gt 0 ]; then
+  if [ $1 = 'setup' ]; then
+    echo '--- setting up your dev environment ---'
+    mkdir -p .key
+    mkdir -p projects
+    mkdir -p services/surrealdb/data/mydata.db
+    cp /home/${USER}/.ssh/id_rsa ./.key/id_rsa
+    cp env.example .env
+    docker compose build
+  fi
 else
-    echo "---> Quick help <----"
-    echo "setup : Setup your environment for the first time".
-    echo "docker compose up : Starts the containers"
-    echo "docker compose exec db bash : ssh into the mysql container"
-    echo "docker compose exec phpfpm bash : ssh into the php environment. This is where you should run composer"
-    echo "docker compose exec nginx bash: ssh into the web server container"
-    echo "docker compse exec workspace bash : ssh into the general container"
+  echo "---> Quick help <----"
+  echo "setup : Setup your environment for the first time".
+  echo "docker compose up : Starts the containers"
+  echo "docker compose exec db bash : ssh into the mysql container"
+  echo "docker compose exec phpfpm bash : ssh into the php environment. This is where you should run composer"
+  echo "docker compose exec nginx bash: ssh into the web server container"
+  echo "docker compse exec workspace bash : ssh into the general container"
 fi
